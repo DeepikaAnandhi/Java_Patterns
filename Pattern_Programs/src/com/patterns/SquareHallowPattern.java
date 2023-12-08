891aa14af42d44161c00aa4a4810daa564b79e1b
deepika <deepika15068@gmail.com>
2023-12-08 11:51:03 +0530

    SquareHallowPattern

---
+++ Pattern_Programs/src/com/patterns/SquareHallowPattern.java
@@ -0,0 +1,46 @@
+package com.patterns;
+
+import java.util.Scanner;
+
+public class SquareHallowPattern {
+
+	public static void createPattern(int row, int col) {
+		
+		int top = 0;
+		int bottom = row-1;
+		
+		int first = 0;
+		int last =col-1;
+		
+		
+		for(int i=0;i<row;i++) {
+				
+			for(int j=0;j<col;j++) {
+				if(i==0 ||i==row-1||j==0 || j==col-1) {
+					System.out.print("x");
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the no of Rows: ");
+		int row = in.nextInt();
+		System.out.print("Enter the no of Columns: ");
+		int col = in.nextInt();
+		
+		createPattern(row,col);
+		
+		
+	}
+	
+	
+}
